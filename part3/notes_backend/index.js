d78cc2f1cbe3f11e0f0f4aad643dfab432b7d236
VP <valtterimj@gmail.com>
2023-11-14 16:26:59 +0200

    part3A

--- part3/notes_backend/index.js
+++ part3/notes_backend/index.js
@@ -2,9 +2,22 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 
-app.use(cors())
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(cors())
 app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('dist'))
 
 let notes = [
     {
@@ -23,6 +36,10 @@ let notes = [
       important: true
     }
   ]
+
+  app.get('/', (req, res) => {
+    res.send('<h1>Hello World!</h1>')
+  })
 
     app.get('/api/notes', (req, res) => {
         res.json(notes)
@@ -72,6 +89,8 @@ let notes = [
     response.json(note)
     
   })
+
+  app.use(unknownEndpoint)
 
   const PORT = process.env.POST || 3001
   app.listen(PORT, () => {
