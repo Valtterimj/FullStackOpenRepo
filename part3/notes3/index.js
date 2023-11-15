24d17312049ed1c53cf11dfb75daf331e7122f49
VP <valtterimj@gmail.com>
2023-11-15 14:09:54 +0200

    part3_exercises

--- part3/notes3/index.js
+++ part3/notes3/index.js
@@ -1,17 +1,26 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
-app.use(express.json())
 
-app.use(requestLogger)
 
 const requestLogger = (request, response, next) => {
-    console.log('Method:', request.method)
-    console.log('Path:  ', request.path)
-    console.log('Body:  ', request.body)
-    console.log('---')
-    next()
-  }
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({ error: 'unknown endpoint' })
+}
+
+
+app.use(cors())
+app.use(express.json())
+app.use(requestLogger)
+app.use(express.static('dist'))
 
 let notes = [
   {
@@ -83,15 +92,11 @@ app.post('/api/notes', (request, response) => {
   
     response.json(note)
 })
-
-const unknownEndpoint = (request, response) => {
-    response.status(404).send({ error: 'unknown endpoint' })
-  }
   
 app.use(unknownEndpoint)
 
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
